304e20e53dbfb3012cff9c24fa962dd6fd838f3d
Butrint Komoni <butrintkomonih@gmail.com>
2019-12-16 15:28:10 +0100

    !
    !

--- pattern_create.py
+++ pattern_create.py
@@ -1,6 +1,5 @@
 import string, argparse, sys
 
-
 def pattern_create():
     parser = argparse.ArgumentParser(description='Pattern Fixer')
     parser.add_argument('-l', '--length', help='The specific length of the pattern', type=int)
@@ -12,16 +11,13 @@ def pattern_create():
         sys.exit(1)
 
     for i in range(1):
-
         pattern = ""
-
+        
         for x in list(string.digits):
             for y in list(string.uppercase):
                 for z in list(string.lowercase):
                     pattern = pattern + str(x) + str(y) + str(z)
         print pattern[:int(length)]
-
-
 
 if __name__ in "__main__":
     pattern_create()
